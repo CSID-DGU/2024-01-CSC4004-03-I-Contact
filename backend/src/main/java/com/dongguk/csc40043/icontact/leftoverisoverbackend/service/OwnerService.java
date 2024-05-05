5be45fc9ab856e6d047eaf047456dce3da2e6298
Jinsoo Yoon <floreo1242@gmail.com>
2024-05-05 22:36:28 +0900

    feat: Owner에 매장 추가 기능 추가

--- backend/src/main/java/com/dongguk/csc40043/icontact/leftoverisoverbackend/service/OwnerService.java
+++ backend/src/main/java/com/dongguk/csc40043/icontact/leftoverisoverbackend/service/OwnerService.java
@@ -1,7 +1,10 @@
 package com.dongguk.csc40043.icontact.leftoverisoverbackend.service;
 
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.domain.Owner;
+import com.dongguk.csc40043.icontact.leftoverisoverbackend.domain.Store;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.repository.OwnerRepository;
+import com.dongguk.csc40043.icontact.leftoverisoverbackend.repository.StoreRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class OwnerService {
 
     private final OwnerRepository ownerRepository;
+    private final StoreRepository storeRepository;
 
     @Transactional
     public Long createOwner(Owner owner) {
@@ -23,7 +27,7 @@ public class OwnerService {
 
     private void validateDuplicateOwner(Owner owner) {
         if (ownerRepository.existsByUsername(owner.getUsername())) {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
+            throw new IllegalStateException("이미 존재하는 점주");
         }
     }
 
@@ -33,6 +37,17 @@ public class OwnerService {
             return owner.getPassword().equals(password);
         }
         return false;
+    }
+
+    @Transactional
+    public Long addStore(Long ownerId, Long storeId) {
+        Owner owner = ownerRepository.findById(ownerId)
+                .orElseThrow(() -> new EntityNotFoundException(ownerId + ": 해당 아이디의 점주 없음"));
+        Store store = storeRepository.findById(storeId)
+                .orElseThrow(() -> new EntityNotFoundException(storeId + ": 해당 아이디의 매장 없음"));
+        owner.getStores().add(store);
+        ownerRepository.save(owner);
+        return ownerId;
     }
 
     @Transactional
--- backend/src/test/java/com/dongguk/csc40043/icontact/leftoverisoverbackend/service/OwnerServiceTest.java
+++ backend/src/test/java/com/dongguk/csc40043/icontact/leftoverisoverbackend/service/OwnerServiceTest.java
@@ -1,7 +1,9 @@
 package com.dongguk.csc40043.icontact.leftoverisoverbackend.service;
 
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.domain.Owner;
+import com.dongguk.csc40043.icontact.leftoverisoverbackend.domain.Store;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.dto.OwnerDto;
+import com.dongguk.csc40043.icontact.leftoverisoverbackend.dto.StoreDto;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.repository.OwnerRepository;
 import jakarta.transaction.Transactional;
 import org.junit.Test;
@@ -9,6 +11,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalTime;
 
 import static org.junit.Assert.*;
 
@@ -21,6 +25,8 @@ public class OwnerServiceTest {
     OwnerService ownerService;
     @Autowired
     OwnerRepository ownerRepository;
+    @Autowired
+    private StoreService storeService;
 
 
     @Test
@@ -43,6 +49,29 @@ public class OwnerServiceTest {
         createOwner();
         boolean authenticated = ownerService.authenticate("floreo1242", "testpasswd");
         assertTrue(authenticated);
+    }
+
+    @Test
+    public void addStore() {
+        OwnerDto ownerDto = new OwnerDto();
+        ownerDto.setUsername("floreo1242");
+        ownerDto.setName("Jinsoo Yoon");
+        ownerDto.setEmail("floreo1242@gmail.com");
+        ownerDto.setPassword("testpasswd");
+        Long ownerId = ownerService.createOwner(ownerDto.toEntity());
+        StoreDto storeDto = new StoreDto();
+        storeDto.setOwner(ownerDto.toEntity());
+        storeDto.setName("Burger King");
+        storeDto.setStartTime(LocalTime.of(9, 0));
+        storeDto.setEndTime(LocalTime.of(21, 0));
+        storeDto.setAddress("Address");
+        storeDto.setPhone("02-1234-5678");
+        Store store = storeDto.toEntity();
+        Long storeId = storeService.createStore(store);
+        ownerService.addStore(ownerId, storeId);
+        Owner savedOwner = ownerRepository.findById(ownerId).orElse(null);
+        assertNotNull(savedOwner);
+        assertTrue(savedOwner.getStores().contains(store));
     }
 
     @Test
