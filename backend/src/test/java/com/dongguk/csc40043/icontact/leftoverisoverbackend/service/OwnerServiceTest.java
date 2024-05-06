dfcf1f49bdfef48c1e444ad422769e356d18203c
Jinsoo Yoon <floreo1242@gmail.com>
2024-05-06 17:18:57 +0900

    test: Dto 등 코드 구성 변경에 따른 테스트 코드 수정

--- backend/src/test/java/com/dongguk/csc40043/icontact/leftoverisoverbackend/service/OwnerServiceTest.java
+++ backend/src/test/java/com/dongguk/csc40043/icontact/leftoverisoverbackend/service/OwnerServiceTest.java
@@ -2,21 +2,18 @@ package com.dongguk.csc40043.icontact.leftoverisoverbackend.service;
 
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.domain.Owner;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.domain.Store;
-import com.dongguk.csc40043.icontact.leftoverisoverbackend.dto.OwnerDto;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.dto.RequestDto.CreateOwnerRequestDto;
+import com.dongguk.csc40043.icontact.leftoverisoverbackend.dto.RequestDto.CreateStoreRequestDto;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.dto.RequestDto.LoginRequestDto;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.dto.ResponseDto.LoginResponseDto;
-import com.dongguk.csc40043.icontact.leftoverisoverbackend.dto.StoreDto;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.repository.OwnerRepository;
+import com.dongguk.csc40043.icontact.leftoverisoverbackend.repository.StoreRepository;
 import jakarta.transaction.Transactional;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.time.LocalTime;
 
 import static org.junit.Assert.*;
 
@@ -31,6 +28,8 @@ public class OwnerServiceTest {
     OwnerRepository ownerRepository;
     @Autowired
     private StoreService storeService;
+    @Autowired
+    private StoreRepository storeRepository;
 
 
     @Test
@@ -66,18 +65,17 @@ public class OwnerServiceTest {
         createOwnerRequestDto.setEmail("floreo1242@gmail.com");
         createOwnerRequestDto.setPassword("testpasswd");
         Long ownerId = ownerService.createOwner(createOwnerRequestDto);
-        StoreDto storeDto = new StoreDto();
-        Owner owner = ownerRepository.findById(ownerId).orElse(null);
-        storeDto.setOwner(owner);
-        storeDto.setName("Burger King");
-        storeDto.setStartTime(LocalTime.of(9, 0));
-        storeDto.setEndTime(LocalTime.of(21, 0));
-        storeDto.setAddress("Address");
-        storeDto.setPhone("02-1234-5678");
-        Store store = storeDto.toEntity();
-        Long storeId = storeService.createStore(store);
+        CreateStoreRequestDto createStoreRequestDto = new CreateStoreRequestDto();
+        createStoreRequestDto.setOwnerId(ownerId);
+        createStoreRequestDto.setName("Burger King");
+        createStoreRequestDto.setStartTime("09:00:00");
+        createStoreRequestDto.setEndTime("18:00:00");
+        createStoreRequestDto.setAddress("Address");
+        createStoreRequestDto.setPhone("02-1234-5678");
+        Long storeId = storeService.createStore(createStoreRequestDto);
         ownerService.addStore(ownerId, storeId);
         Owner savedOwner = ownerRepository.findById(ownerId).orElse(null);
+        Store store = storeRepository.findById(storeId).orElse(null);
         assertNotNull(savedOwner);
         assertTrue(savedOwner.getStores().contains(store));
     }
@@ -92,7 +90,7 @@ public class OwnerServiceTest {
         Long savedId = ownerService.createOwner(createOwnerRequestDto);
 
         ownerService.deleteOwner(savedId);
-        
+
         Owner owner = ownerRepository.findById(savedId).orElse(null);
         assertNull(owner);
     }
