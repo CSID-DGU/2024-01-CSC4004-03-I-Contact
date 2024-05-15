6723805b2ae0183f811be62f0774dd2f234d7dd6
Jinsoo Yoon <floreo1242@gmail.com>
2024-05-15 14:08:23 +0900

    feat: 회원삭제 API 구현

--- backend/src/main/java/com/dongguk/csc40043/icontact/leftoverisoverbackend/controller/MemberController.java
+++ backend/src/main/java/com/dongguk/csc40043/icontact/leftoverisoverbackend/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.dongguk.csc40043.icontact.leftoverisoverbackend.controller;
 
+import com.dongguk.csc40043.icontact.leftoverisoverbackend.common.SecurityUtil;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.dto.RequestDto.member.CheckDuplicateRequestDto;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.dto.RequestDto.member.CreateMemberRequestDto;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.dto.RequestDto.member.LoginRequestDto;
@@ -7,10 +8,9 @@ import com.dongguk.csc40043.icontact.leftoverisoverbackend.service.MemberService
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -43,6 +43,19 @@ public class MemberController {
         try {
             memberService.checkDuplicate(checkDuplicateRequestDto);
             return ResponseEntity.status(HttpStatus.OK).body("Available username");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/member")
+    public ResponseEntity<?> deleteMember() {
+        try {
+            String username = SecurityUtil.getCurrentUser();
+            memberService.deleteMember(username);
+            return ResponseEntity.status(HttpStatus.OK).body("Member deleted successfully");
+        } catch (UsernameNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
--- backend/src/main/java/com/dongguk/csc40043/icontact/leftoverisoverbackend/service/MemberService.java
+++ backend/src/main/java/com/dongguk/csc40043/icontact/leftoverisoverbackend/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.dongguk.csc40043.icontact.leftoverisoverbackend.service;
 
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.common.JwtTokenProvider;
+import com.dongguk.csc40043.icontact.leftoverisoverbackend.domain.Member;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.dto.MemberDto;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.dto.RequestDto.member.CheckDuplicateRequestDto;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.dto.RequestDto.member.LoginRequestDto;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -43,8 +45,17 @@ public class MemberService {
         isDuplicated(checkDuplicateRequestDto.getUsername());
     }
 
+    @Transactional
+    public void deleteMember(String username) {
+        Member member = memberRepository.findByUsernameAndDeleted(username, false);
+        if (member == null) {
+            throw new UsernameNotFoundException("해당하는 회원을 찾을 수 없습니다.");
+        }
+        memberRepository.delete(member);
+    }
+
     void isDuplicated(String username) {
-        if (memberRepository.existsByUsername(username)) {
+        if (memberRepository.existsByUsernameAndDeleted(username, false)) {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         }
     }
