ee546fdafeb6262b85cf99dfcb9d445091d6e894
Jinsoo Yoon <floreo1242@gmail.com>
2024-05-21 14:02:19 +0900

    feat: 식당별 매뉴 조회 api 구현

--- backend/src/main/java/com/dongguk/csc40043/icontact/leftoverisoverbackend/controller/FoodController.java
+++ backend/src/main/java/com/dongguk/csc40043/icontact/leftoverisoverbackend/controller/FoodController.java
@@ -5,6 +5,7 @@ import com.dongguk.csc40043.icontact.leftoverisoverbackend.service.FoodService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,15 @@ public class FoodController {
     public ResponseEntity<?> addFood(@RequestBody AddFoodRequestDto addFoodRequestDto) {
         try {
             return ResponseEntity.status(HttpStatus.CREATED).body(foodService.addFood(addFoodRequestDto));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/food")
+    public ResponseEntity<?> getFoodList() {
+        try {
+            return ResponseEntity.ok(foodService.getFoodList());
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
--- backend/src/main/java/com/dongguk/csc40043/icontact/leftoverisoverbackend/service/FoodService.java
+++ backend/src/main/java/com/dongguk/csc40043/icontact/leftoverisoverbackend/service/FoodService.java
@@ -6,12 +6,16 @@ import com.dongguk.csc40043.icontact.leftoverisoverbackend.domain.Member;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.domain.Store;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.dto.RequestDto.food.AddFoodRequestDto;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.dto.ResponseDto.AddFoodResponseDto;
+import com.dongguk.csc40043.icontact.leftoverisoverbackend.dto.ResponseDto.GetFoodListResponseDto;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.repository.FoodRepository;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.repository.MemberRepository;
 import com.dongguk.csc40043.icontact.leftoverisoverbackend.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -30,6 +34,25 @@ public class FoodService {
         return AddFoodResponseDto.builder()
                 .foodId(food.getId())
                 .build();
+    }
+
+    public List<GetFoodListResponseDto> getFoodList() {
+        Member member = memberRepository.findByUsernameAndDeleted(SecurityUtil.getCurrentUser(), false);
+        Store store = storeRepository.findByMemberAndDeleted(member, false);
+        return store.getFoods().stream()
+                .map(food -> GetFoodListResponseDto.builder()
+                        .foodId(food.getId())
+                        .storeId(store.getId())
+                        .name(food.getName())
+                        .firstPrice(food.getFirstPrice())
+                        .sellPrice(food.getSellPrice())
+                        .capacity(food.getCapacity())
+                        .visits(food.getVisits())
+                        .isVisible(food.isVisible())
+                        .deleted(food.isDeleted())
+                        .build()
+                )
+                .collect(Collectors.toList());
     }
 
 }
