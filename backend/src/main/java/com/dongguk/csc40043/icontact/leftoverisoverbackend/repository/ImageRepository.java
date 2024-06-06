f65da1f03b80ae983e8f36bb8074f1d53de7b3c4
Jinsoo Yoon <floreo1242@gmail.com>
2024-06-07 01:20:34 +0900

    feat: 메뉴 이미지 구현
    등록, 수정시 이미지 포함 가능
    gitignore에 이미지 저장 폴더 추가

---
+++ backend/src/main/java/com/dongguk/csc40043/icontact/leftoverisoverbackend/repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package com.dongguk.csc40043.icontact.leftoverisoverbackend.repository;
+
+import com.dongguk.csc40043.icontact.leftoverisoverbackend.domain.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
