18435ba6aaa4f3bdb4101515fa94552beecabd2e
Jinsoo Yoon <floreo1242@gmail.com>
2024-06-10 19:38:27 +0900

    fix: 가게 조회시 거리 제한 설정
    2000미터

--- backend/src/main/java/com/dongguk/csc40043/icontact/leftoverisoverbackend/repository/StoreRepository.java
+++ backend/src/main/java/com/dongguk/csc40043/icontact/leftoverisoverbackend/repository/StoreRepository.java
@@ -16,10 +16,10 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
 
     List<Store> findByNameContaining(String name);
 
-    @Query(value = "SELECT *, ST_Distance_Sphere(point(longitude, latitude), point(:targetLon, :targetLat)) as distance FROM store ORDER BY distance", nativeQuery = true)
+    @Query(value = "SELECT *, ST_Distance_Sphere(point(longitude, latitude), point(:targetLon, :targetLat)) as distance FROM store WHERE ST_Distance_Sphere(point(longitude, latitude), point(:targetLon, :targetLat)) <= 2000 ORDER BY distance", nativeQuery = true)
     List<Store> findAllSortedByDistance(double targetLat, double targetLon);
 
-    @Query(value = "SELECT *, ST_Distance_Sphere(point(longitude, latitude), point(:targetLon, :targetLat)) as distance FROM store WHERE category_id = :categoryId ORDER BY distance", nativeQuery = true)
+    @Query(value = "SELECT *, ST_Distance_Sphere(point(longitude, latitude), point(:targetLon, :targetLat)) as distance FROM store WHERE category_id = :categoryId AND ST_Distance_Sphere(point(longitude, latitude), point(:targetLon, :targetLat)) <= 2000 ORDER BY distance", nativeQuery = true)
     List<Store> findByCategoryIdSortedByDistance(Long categoryId, double targetLat, double targetLon);
 
     Optional<Store> findByIdAndDeleted(Long id, boolean deleted);
